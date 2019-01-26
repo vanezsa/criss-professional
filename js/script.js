59ffa2e2860725f5a9c5c72b90a8287f556b2932
vanezsa <34085957+vanezsa@users.noreply.github.com>
2019-01-26 13:46:14 +0800

    Responsive
    Responsive yay!

---
+++ js/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    $(".input").focus(function() {
+        $(this).parent().addClass("focus");
+    })
+
+    
+    $(document).ready(function() {
+        // Transition effect for navbar 
+        $(window).scroll(function() {
+          // checks if window is scrolled more than 500px, adds/removes solid class
+          if($(this).scrollTop() > 200) { 
+              $('.navbar').addClass('solid');
+          } else {
+              $('.navbar').removeClass('solid');
+          }
+        });
+});
+    
+});
